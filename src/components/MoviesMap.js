6471d1c02aed3d1fa9e44c8e4b13cf7387ca0118
Unicar <unicar9@gmail.com>
2018-08-12 23:30:04 +1000

    Added marker

--- src/components/MoviesMap.js
+++ src/components/MoviesMap.js
@@ -3,35 +3,15 @@ import { withScriptjs, withGoogleMap, GoogleMap } from 'react-google-maps';
 import MovieMarker from "./MovieMarker";
 
 const MoviesMap = withScriptjs(withGoogleMap((props) => {
-    // const movies = [
-    //     {
-    //         key: 1,
-    //         movie: 'Mission Impossible',
-    //         location: { lat: 37.49820, lng: -24.01507 }
-    //     },
-    //     {
-    //         key: 2,
-    //         movie: 'Mission Impossible',
-    //         location: { lat: 37.49820, lng: -24.01507 }
-    //     }
-    // ]
-
-    // const markers = movies.map(movie => <MovieMarker
-    //                     key={movie.key}
-    //                     movie={movie.movie}
-    //                     location={movie.location}
-    //                 />);
-
-                    
-    return (
+        return (
             <GoogleMap
-                defaultZoom={14}
+                defaultZoom={10}
                 defaultCenter={{ lat: -33.86785, lng: 151.20732 }}
             >
                 {(props.markers || []).map(cinema => 
                     <MovieMarker
                         key={cinema.id}
-                        location={cinema.location}
+                        location={{ lat: cinema.location.lat, lng: cinema.location.lon }}
                     />
                 )}
             </GoogleMap>
